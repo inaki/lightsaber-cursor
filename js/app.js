366a4a29b6f8690a233826ad1576018977214cbb
Keala Lusk <contactkeala@gmail.com>
2016-02-26 19:05:55 -0800

    changes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
-/*
 $(function () {
   $("button").click(function() {
-    $("body").addClass('lightsaber-cursor');
+    $("body").css({'cursor': "url('http://i.imgur.com/Fg4fMvn.png?1'), default"});
   });
 });
-*/
